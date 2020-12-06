8091ecc9c2c604d69c685bb85f4837dcc62abd3f
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-06 11:34:53 -0300

    modified component added a h1

--- src/Components/Headers.jsx
+++ src/Components/Headers.jsx
@@ -1,20 +1,14 @@
-import React, { Components } from 'react'
+import React, { Component } from 'react';
+import logo from '../logo.svg';
 
-export default class Headers extends Components {
+
+export default class Headers extends Component {
     render() {
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        return(
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Hello</h1>
+            </header>
+        )
     }
-}
+};
