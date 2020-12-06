fd133eb478e9ea862b5ee8733b760f80a913ebd6
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-06 13:47:49 -0300

    created a state to change initial text using onClick

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import Headers from './Components/Headers';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    miau: 'Bienvenido a miau'
+  }
+
+  cambiarTextoDelEstado = () => {
+    this.setState({ miau: 'Hola mundo' })
+  }
+
   manejaClick = texto => {
     console.log(texto)
   }
   render() {
-    const miau = 'Welcome Miau'
+    const { miau } = this.state
+    const texto = 'Welcome Miau'
     return (
       <div className="App">
-        <Headers miau={ miau } manejaClick={ this.manejaClick } />
+        <Headers miau={ texto } manejaClick={ this.manejaClick } />
+        <p onClick={ this.cambiarTextoDelEstado }  className="App-intro">
+          { miau }
+        </p>
       </div>
     )
   } 
