29fb4c3cd7029de307f4655881a9293427b5c4e2
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-06 11:59:56 -0300

    this props added to extend properties

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Headers />
+      <Headers miau={'Welcome Miau'} />
     </div>
   );
 }
--- src/Components/Headers.jsx
+++ src/Components/Headers.jsx
@@ -4,10 +4,11 @@ import logo from '../logo.svg';
 
 export default class Headers extends Component {
     render() {
+        const { miau } = this.props
         return(
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Hello</h1>
+        <h1 className="App-title">{ miau }</h1>
             </header>
         )
     }
