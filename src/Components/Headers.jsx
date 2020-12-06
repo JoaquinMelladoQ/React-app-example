fb4484e60e951def37083d39b09a8c711c07fe49
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-06 19:15:36 -0300

    defining styles and saved it in a constant

--- src/Components/Headers.jsx
+++ src/Components/Headers.jsx
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 
 
+const styles = {
+    header: {
+        backgroundColor: '#282c34',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 'calc(10px + 2vmin)',
+        color: 'white',
+    }
+}
+
 export default class Headers extends Component {
 
     manejaClick = () => {
@@ -12,7 +25,7 @@ export default class Headers extends Component {
     render() {
         const { miau, manejaClick } = this.props
         return(
-            <header className="App-header">
+            <header style={styles.header}>
                 <img src={logo} className="App-logo" alt="logo" />
                 <h1 onClick={ this.manejaClick } className="App-title">{ miau }</h1>
             </header>
