dd8c6bb0adccf484f2f806da934de45c30e3ee1c
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-06 12:40:01 -0300

    onclick event was added to h1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import React, { Component } from 'react';
 import Headers from './Components/Headers';
 import './App.css';
 
-function App() {
-  const miau = 'Welcome Miau'
-  return (
-    <div className="App">
-      <Headers miau={ miau } />
-    </div>
-  );
+class App extends Component {
+  manejaClick = () => {
+    console.log('he sido clickeado!!!!!')
+  }
+  render() {
+    const miau = 'Welcome Miau'
+    return (
+      <div className="App">
+        <Headers miau={ miau } manejaClick={ this.manejaClick } />
+      </div>
+    )
+  } 
 }
 
-export default App;
+
+
+export default App
--- src/Components/Headers.jsx
+++ src/Components/Headers.jsx
@@ -4,12 +4,12 @@ import logo from '../logo.svg';
 
 export default class Headers extends Component {
     render() {
-        const { miau } = this.props
+        const { miau, manejaClick } = this.props
         return(
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">{ miau }</h1>
+                <h1 onClick={ manejaClick } className="App-title">{ miau }</h1>
             </header>
         )
     }
-};
+}
