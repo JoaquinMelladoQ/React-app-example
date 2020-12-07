c43510183e9840e10a1d1e55d1a6688b6ac4afe2
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-07 20:53:31 -0300

    post method using fetch and then

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import FormularioSimple from './Components/FormularioSimple'
 class App extends Component {
   constructor(){
     super()
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(x => x.json())
-      .then(x => console.log(x))
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then(x => x.json())
+    //   .then(x => console.log(x))
+
+    fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: 'Joaquin Mellado',
+        username: 'jom',
+      })
+    }).then(x => x.json())
+    .then(x => console.log(x))
+
   }
 
   render() {
