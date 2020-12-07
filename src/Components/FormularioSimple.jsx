3369a44870823e096beb492802e8b2c4208bf0ae
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-07 18:03:31 -0300

    added a validation at form

--- src/Components/FormularioSimple.jsx
+++ src/Components/FormularioSimple.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import Parag from './Parag'
+
 
 export default class FormularioSimple extends Component {
 
-    state = {}
+    state = {
+        errors: {
+            nombre: 'campo obligatorio'
+        }
+    }
 
     handleChange = ({ target }) => {
         const { name, value } = target
@@ -11,14 +17,16 @@ export default class FormularioSimple extends Component {
 
     handleSubmit = e => {
         e.preventDefault() 
-        console.log('prevenido!!!!')
+        console.log('prevenido!', this.state)
     }
 
     render() {
+        const { errors } = this.state
         console.log(this.state)
         return (
             <form onSubmit={this.handleSubmit} >
                 <input name="nombre" onChange={this.handleChange} />
+                    {errors.nombre && <Parag>{errors.nombre}</Parag>}
                 <input name="apellido" onChange={this.handleChange} />
                 <input type="submit" value="Enviar"/>
             </form>
