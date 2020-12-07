c0bb2db711cc51b7678277ff917478fc4119b565
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-06 21:01:46 -0300

    added a form and handle it with handleChange

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import Headers from './Components/Headers';
-import './App.css';
-import Parag from './Components/Parag';
+import FormularioSimple from './Components/FormularioSimple'
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        Hola mundo
+        <FormularioSimple  />
       </div>
     )
   } 
